25d7bd3940db68a2edbc6a4bebbe14c9112f9fa1
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-20 00:24:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {
   swizzBeatz: 'Poison',
   nas: 'Hate Me Now'
 }
+
+function updatPlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
