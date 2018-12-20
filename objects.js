e97f21666c0eea4229fd1195c9a32724a97b91b3
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-20 00:52:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  const playlist2 = delete playlist.artistName
-  return playlist2
+  delete playlist.artistName
+  return playlist
 }
